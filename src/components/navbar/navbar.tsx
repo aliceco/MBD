351c1471a7a0e3b3bbc9722aa58e4b9d621a4e61
aliceco <aliceco@kth.se>
2025-11-12 20:56:46 +0100

    Fixed the new navbar
    New navbar with dropdown menus instead, created a new component in navbar.
    
    Mobile version doesn't work as it should
    
    (Also fixed Material-UI issue)

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,47 +5,24 @@ import Routes from '../../routes/routes'
 import { NavLink } from 'react-router-dom'
 
 import mbdLogo from '../../assets/mbd-logo/mbd-logo-yellow.svg'
-import kthLogo from '../../assets/KTH_logo.png'
-import medieteknikLogo from '../../assets/medieteknik_logo.png'
+
 import HamburgerButton from '../hamburger-button/hamburger-button'
-import InstagramIconWhite from '../../assets/icons/other/instagram_white.png'
-import FacebookIconWhite from '../../assets/icons/other/facebook_white.png'
-import LinkedIconWhite from '../../assets/icons/other/linked_in_white.png'
-import InstagramIconYellow from '../../assets/icons/other/instagram_icon_yellow.png'
-import FacebookIconYellow from '../../assets/icons/other/facebook_icon_yellow.png'
-import LinkedinIconYellow from '../../assets/icons/other/linkedin_icon_yellow.png'
 
 import TranslationModel from '../../pages/model/translationModel'
 import LanguageSelect from './language-select/language-select'
 import { Line, LineType } from '../lines/line'
+import phrases from '../../data/translations.json'
+
+import NavItem from '../nav-item/nav-item'
 
 const Navbar = () => {
-    const [mobileMenuOpen, toogleMobileMenuOpen] = useState(false)
-    const [instagranIcon, setInstagranIcon] = useState(InstagramIconWhite)
-    const [FacebookIcon, setFacebookIcon] = useState(FacebookIconWhite)
-    const [LinkedInIcon, setLinkedInIcon] = useState(LinkedIconWhite)
-
-    const openFacebook = () => {
-        window.open(
-            'https://www.facebook.com/mediasbranschdag',
-            'Medias Branschdag Facebook'
-        )
-    }
-    const openInstagram = () => {
-        window.open(
-            'https://www.instagram.com/mediasbranschdag/',
-            'Medias Branschdag Instagram'
-        )
-    }
-    const openLinkedin = () => {
-        window.open(
-            'https://www.linkedin.com/company/medias-branschdag/',
-            'Medias Branschdag LinkedIn'
-        )
-    }
+    const [mobileMenuOpen, togleMobileMenuOpen] = useState(false)
 
     return (
         <div className='navbar'>
+            {/* First part of navbar */}
+
+            {/*MBD logo */}
             <div className='navbar-leading navbar-padding'>
                 <NavLink className='no-select' exact to='/'>
                     <img
@@ -55,68 +32,95 @@ const Navbar = () => {
                     />
                 </NavLink>
             </div>
+            {/* Navbar texts */}
             <div
                 className={`navbar-content navbar-padding ${
                     mobileMenuOpen ? 'active' : ''
                 }`}
             >
-                {Object.values(Routes)
-                    .filter((route) => route.inMenu)
-                    .map((route) => {
-                        return (
-                            <NavLink
-                                exact
-                                className='navbar-link'
-                                activeClassName='navbar-link active'
-                                onClick={() =>
-                                    toogleMobileMenuOpen(!mobileMenuOpen)
-                                }
-                                to={route.path}
-                                key={route.path}
-                            >
-                                {TranslationModel.translate(route.name)}
-                            </NavLink>
-                        )
-                    })}
+               {/* Home */}
+                <NavItem
+                    label = {TranslationModel.translate(Routes.homePage.name)}
+                    path = {Routes.homePage.path}
+                    items = {[]} 
+                />
+
+                {/* Company hpver fpr the menu 
+                 Current issues:
+                - Very long and repetetive code SOLVED
+                - Won't work well on mobile so should add a conditional rendering that checks
+                  if you're on mobile
+                - Have created a component but no idea how to do that SOLVED
+                - Can we make the entire thing a component? like the whole div? SOLVED
+                */}
+                {/* For Companies */}
+                <NavItem 
+                    label = {TranslationModel.translate(Routes.companyPage.name)}
+                    path = {Routes.companyPage.path}
+                    items={[
+                        {
+                            label: TranslationModel.translate(Routes.whatIsPage.name),
+                            path: Routes.whatIsPage.path
+                        },
+                        {
+                            label: TranslationModel.translate(phrases.exhibitor_packages),
+                            path: Routes.companyPage.path
+                        },
+                        {
+                            label: TranslationModel.translate(Routes.contactPage.name),
+                            path: Routes.contactPage.path
+                        }
+                    ]}
+                />
+
+                {/* For Students */}
+                <NavItem 
+                    label = {TranslationModel.translate(phrases.for_students)}
+                    path = {''} // doesn't have a page yet
+                    items={[
+                        {
+                            label: TranslationModel.translate(Routes.studentPage.name),
+                            path: Routes.studentPage.path
+                        },
+                        {
+                            label: TranslationModel.translate(Routes.mapPage.name),
+                            path: Routes.mapPage.path
+                        },
+                        {
+                            label: TranslationModel.translate(phrases.for_students),
+                            path: '' // This page doesn't exist yet
+                        }
+                    ]}
+                />
+
+               
             </div>
+
+            {/* Language flag */}
             <div className='navbar-trailing navbar-padding'>
+                <NavItem
+                    label = {TranslationModel.translate(Routes.contactPage.name)}
+                    path = {Routes.contactPage.path}
+                    items = {[]} 
+                />
+                {/* Om oss */}
+               <NavItem
+                    label = {TranslationModel.translate(phrases.about_us)}
+                    path = {''} //Does not yet have a page
+                    items = {[]} 
+                />
+                <div className='navbar-trailing-item'>
+                    <Line lineType={LineType.vertical} />
+                </div>
                 <div className='navbar-trailing-item'>
                     <LanguageSelect />
                 </div>
 
-                <div className='navbar-trailing-item'>
-                    <Line lineType={LineType.vertical} />
-                </div>
-
-                <img
-                    className='side-logo navbar-trailing-item '
-                    src={instagranIcon}
-                    onMouseEnter={() => setInstagranIcon(InstagramIconYellow)}
-                    onMouseOut={() => setInstagranIcon(InstagramIconWhite)}
-                    alt=''
-                    onClick={openInstagram}
-                />
-                <img
-                    className='side-logo navbar-trailing-item '
-                    src={FacebookIcon}
-                    onMouseEnter={() => setFacebookIcon(FacebookIconYellow)}
-                    onMouseOut={() => setFacebookIcon(FacebookIconWhite)}
-                    alt=''
-                    onClick={openFacebook}
-                />
-                <img
-                    className='side-logo navbar-trailing-item '
-                    src={LinkedInIcon}
-                    onMouseEnter={() => setLinkedInIcon(LinkedinIconYellow)}
-                    onMouseOut={() => setLinkedInIcon(LinkedIconWhite)}
-                    alt=''
-                    onClick={openLinkedin}
-                />
-
+                {/* Line */}
                 <div className='navbar-mobile-menu-button navbar-trailing-item '>
                     <HamburgerButton
                         onClick={() => {
-                            toogleMobileMenuOpen(!mobileMenuOpen)
+                            togleMobileMenuOpen(!mobileMenuOpen)
                         }}
                         isActive={mobileMenuOpen}
                     />
@@ -127,3 +131,82 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+//Old code when the links to social media was in the navbar
+
+// import kthLogo from '../../assets/KTH_logo.png'
+// import medieteknikLogo from '../../assets/medieteknik_logo.png'
+// import InstagramIconWhite from '../../assets/icons/other/instagram_white.png'
+// import FacebookIconWhite from '../../assets/icons/other/facebook_white.png'
+// import LinkedIconWhite from '../../assets/icons/other/linked_in_white.png'
+// import InstagramIconYellow from '../../assets/icons/other/instagram_icon_yellow.png'
+// import FacebookIconYellow from '../../assets/icons/other/facebook_icon_yellow.png'
+// import LinkedinIconYellow from '../../assets/icons/other/linkedin_icon_yellow.png'
+
+// const [instagranIcon, setInstagranIcon] = useState(InstagramIconWhite)
+// const [FacebookIcon, setFacebookIcon] = useState(FacebookIconWhite)
+// const [LinkedInIcon, setLinkedInIcon] = useState(LinkedIconWhite)
+
+// const openFacebook = () => {
+//     window.open(
+//         'https://www.facebook.com/mediasbranschdag',
+//         'Medias Branschdag Facebook'
+//     )
+// }
+// const openInstagram = () => {
+//     window.open(
+//         'https://www.instagram.com/mediasbranschdag/',
+//         'Medias Branschdag Instagram'
+//     )
+// }
+// const openLinkedin = () => {
+//     window.open(
+//         'https://www.linkedin.com/company/medias-branschdag/',
+//         'Medias Branschdag LinkedIn'
+//     )
+// }
+
+// <img
+//                 className='side-logo navbar-trailing-item '
+//                 src={instagranIcon}
+//                 onMouseEnter={() => setInstagranIcon(InstagramIconYellow)}
+//                 onMouseOut={() => setInstagranIcon(InstagramIconWhite)}
+//                 alt=''
+//                 onClick={openInstagram}
+//             />
+//             <img
+//                 className='side-logo navbar-trailing-item '
+//                 src={FacebookIcon}
+//                 onMouseEnter={() => setFacebookIcon(FacebookIconYellow)}
+//                 onMouseOut={() => setFacebookIcon(FacebookIconWhite)}
+//                 alt=''
+//                 onClick={openFacebook}
+//             />
+//             <img
+//                 className='side-logo navbar-trailing-item '
+//                 src={LinkedInIcon}
+//                 onMouseEnter={() => setLinkedInIcon(LinkedinIconYellow)}
+//                 onMouseOut={() => setLinkedInIcon(LinkedIconWhite)}
+//                 alt=''
+//                 onClick={openLinkedin}
+//             />
+
+/* --- Previous code where all pages were shown directly on the navbar --- */
+//  {Object.values(Routes)
+//                         .filter((route) => route.inMenu)
+//                         .map((route) => {
+//                                 return (
+//                                     <NavLink
+//                                         exact
+//                                         className='navbar-link'
+//                                         activeClassName='navbar-link active'
+//                                         onClick={() =>
+//                                             toogleMobileMenuOpen(!mobileMenuOpen)
+//                                         }
+//                                         to={route.path}
+//                                         key={route.path}
+//                                     >
+//                                         {TranslationModel.translate(route.name)}
+//                                     </NavLink>
+//                                 )   
+//                         })}
